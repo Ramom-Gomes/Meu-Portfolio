2a0bfbb16402a77e33aa12e7e6b10b5d75d3dba7
Ramom Gomes <ramomusica09@hotmail.com>
2023-04-03 22:06:03 -0300

    atulizacao-responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const app = () => {
           <div className="informacoes">
             <div className="informacoespessoais">
               <h1 className="informacoestitulo">Desenvolvedor Front-End React 👋🏿</h1>
-              <p className="informacoesdescricao">Olá. Eu sou o Ramom. Um apaixado Desenvolvedor Front-End React.</p>
+              <p className="informacoesdescricao">Olá. Eu sou o Ramom. Um apaixonado Desenvolvedor Front-End React.</p>
               <div className="portfolio">
                 <a target={"_blank"} href="https://www.linkedin.com/in/ramomgomes/"><img className="redesimage" src={FotoLin} alt="" /></a>
                 <a target={"_blank"} href="https://github.com/Ramom-Gomes"><img className="redesimage" src={FotoGit} alt="" /></a>
